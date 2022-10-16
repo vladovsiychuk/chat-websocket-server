506b916a726a6a6783a2864acc83f96fefe9122b
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2022-10-16 11:57:39 +0300

    apply formatting

--- src/main/java/com/involveininnovation/chat/entity/Message.java
+++ src/main/java/com/involveininnovation/chat/entity/Message.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Entity
 public class Message {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String sender;
     private String receiver;
