85547fd3066f13d8185b4320eb9c58063056b117
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-03-22 23:54:03 +0200

    apply formatting

--- src/main/java/com/involveininnovation/chat/config/WebSocketConfig.java
+++ src/main/java/com/involveininnovation/chat/config/WebSocketConfig.java
@@ -11,13 +11,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/chatroom","/user");
+        registry.enableSimpleBroker("/chatroom", "/user");
         registry.setUserDestinationPrefix("/user");
     }
 }
